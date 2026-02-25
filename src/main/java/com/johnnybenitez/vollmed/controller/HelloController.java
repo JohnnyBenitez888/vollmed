958fbd86da3661637b08a92547718dbf42448c45
Johnny Benitez <johnnybeniitez@gmail.com>
2026-02-24 21:38:56 -0300

    First Commit

---
+++ src/main/java/com/johnnybenitez/vollmed/controller/HelloController.java
@@ -0,0 +1,4 @@
+package com.johnnybenitez.vollmed.controller;
+
+public class HelloController {
+}
---
+++ src/test/java/com/johnnybenitez/vollmed/VollmedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnnybenitez.vollmed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VollmedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
